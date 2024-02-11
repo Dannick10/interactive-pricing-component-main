9044df719e674b637bdda662b945062df542ce2b
Daniel Rocha <102672001+Dannick10@users.noreply.github.com>
2024-02-11 20:30:21 -0300

    atualização

--- script.js
+++ script.js
@@ -1,4 +1,19 @@
 const rangeInputs = document.querySelectorAll('input[type="range"]')
+const check = document.querySelector('input[type="checkbox"')
+const div_moth = document.querySelector('.value_m')
+
+
+check.addEventListener('click',()=>{
+    if(check.checked){
+        div_moth.innerHTML = '/ yearly'
+        console.log(rangeInputs)
+    }else{
+        div_moth.innerHTML = '/ month'
+    }
+
+})
+
+
 
 function handleInputChange(e) {
   let target = e.target
@@ -7,11 +22,15 @@ function handleInputChange(e) {
   } 
   const min = target.min
   const max = target.max
-  const val = target.value
+    const val = target.value
   
   target.style.backgroundSize = (val - min) * 100 / (max - min) + '% 100%'
-
+  if(!check.checked){
+    
   document.querySelector('.value_money').innerHTML = `$ ${val}.00`
+  }else{
+    document.querySelector('.value_money').innerHTML = `$ ${val*3}.00`
+  }
 }
 
 rangeInputs.forEach(input => {
