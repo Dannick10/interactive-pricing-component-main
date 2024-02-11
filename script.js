eec6e7ba46abfc139987a679b313984baba4be9f
Daniel Rocha <102672001+Dannick10@users.noreply.github.com>
2024-02-11 20:58:18 -0300

    fix bug

--- script.js
+++ script.js
@@ -1,14 +1,16 @@
 const rangeInputs = document.querySelectorAll('input[type="range"]')
 const check = document.querySelector('input[type="checkbox"')
 const div_moth = document.querySelector('.value_m')
-
+const value = document.querySelector('.value_money')
 
 check.addEventListener('click',()=>{
     if(check.checked){
         div_moth.innerHTML = '/ yearly'
-        console.log(rangeInputs)
+        value.innerHTML = value.innerHTML*2 + '.00'
+
     }else{
         div_moth.innerHTML = '/ month'
+        value.innerHTML = value.innerHTML/2 + '.00'
     }
 
 })
@@ -27,9 +29,9 @@ function handleInputChange(e) {
   target.style.backgroundSize = (val - min) * 100 / (max - min) + '% 100%'
   if(!check.checked){
     
-  document.querySelector('.value_money').innerHTML = `$ ${val}.00`
+  document.querySelector('.value_money').innerHTML = `${val}.00`
   }else{
-    document.querySelector('.value_money').innerHTML = `$ ${val*3}.00`
+    document.querySelector('.value_money').innerHTML = `${val*2}.00`
   }
 }
 
