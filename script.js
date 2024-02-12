5858d05f722e38eb3172c69582bee1a4c5fb873a
Daniel Rocha <102672001+Dannick10@users.noreply.github.com>
2024-02-12 00:09:24 -0300

    pageview e padding

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const rangeInputs = document.querySelectorAll('input[type="range"]')
 const check = document.querySelector('input[type="checkbox"')
 const div_moth = document.querySelector('.value_m')
 const value = document.querySelector('.value_money')
+const view = document.querySelector('.card-title h2 span')
 
 check.addEventListener('click',()=>{
     if(check.checked){
@@ -24,15 +25,43 @@ function handleInputChange(e) {
   } 
   const min = target.min
   const max = target.max
-    const val = target.value
+  const val = target.value
+
   
   target.style.backgroundSize = (val - min) * 100 / (max - min) + '% 100%'
-  if(!check.checked){
-    
+
+  pageView(val)
+
+  if(!check.checked){ 
   document.querySelector('.value_money').innerHTML = `${val}.00`
   }else{
     document.querySelector('.value_money').innerHTML = `${val*2}.00`
   }
+}
+
+
+function pageView(val){
+  let res
+  switch(val){
+    case '0':
+      res = '0'
+      break;
+    case '8' || '16':
+      res = 10
+      break;
+    case '16' || '32':
+     res = 50
+    break;
+
+    case '24' || '48':
+      res = 100
+    break;
+
+     case '32' || '64':
+      res = 500
+  }
+
+  view.innerHTML = res + 'k'
 }
 
 rangeInputs.forEach(input => {
